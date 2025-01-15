ec38522df7d9276f0ee324ac689699059154b391
Loïse <izakhaak@gmail.com>
2025-01-15 20:37:40 +0100

    #2 redid transaction layout

---
+++ TRAFO.Logic/Transaction/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace TRAFO.Logic;
+
+public sealed record Transaction
+{
+    public required int Amount { get; init; }
+    public required Currency Currency { get; init; }
+    public required string OtherPartyName { get; init; }
+    public required DateTime Timestamp { get; init; }
+    public required string RawData { get; init; }
+    public required Category Category { get; init; }
+}
