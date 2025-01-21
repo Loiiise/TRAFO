3c329152d8d1b86cf70d2e42444994f64621dd9d
Loïse <izakhaak@gmail.com>
2025-01-21 19:33:07 +0100

    #2 added input validating tests

---
+++ TRAFO.Logic/Extensions/EnumExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace TRAFO.Logic.Extensions;
+
+public static class EnumExtensions
+{
+    public static IEnumerable<T> GetAllValues<T>()
+        where T : Enum
+        => Enum.GetValues(typeof(T)).Cast<T>();
+}
--- Tests/TRAFO.Parsing.Tests/CSVParserTests.cs
+++ Tests/TRAFO.Parsing.Tests/CSVParserTests.cs
@@ -1,5 +1,6 @@
 ﻿using Shouldly;
 using TRAFO.Logic;
+using TRAFO.Logic.Extensions;
 
 namespace TRAFO.Parsing.Tests;
 
@@ -12,7 +13,7 @@ public class CSVParserTests
 
         var result = parser.Parse(transactionLine);
 
-        result.ShouldNotBeNull();        
+        result.ShouldNotBeNull();
         result.Amount.ShouldBe(transaction.Amount);
         result.Currency.ShouldBe(transaction.Currency);
         result.OtherPartyName.ShouldBe(transaction.OtherPartyName);
@@ -46,28 +47,115 @@ public class CSVParserTests
         }
     }
 
-    public static IEnumerable<object[]> GenerateLegalStringAndTransactionObjects()
-        => GenerateLegalStringsAndTransactions().Select(st => new object[] { st.Item1, st.Item2 });
-    private static IEnumerable<(string, Transaction)> GenerateLegalStringsAndTransactions()
+    [Theory, MemberData(nameof(GenerateLegalTransactionObjects))]
+    public void AmountShouldAlwaysBeANumber(Transaction transaction)
     {
-        var sep = MockCSVParser.DefaultSeparator;
+        var parser = new MockCSVParser();
 
+        // Get legal values for all other fields
+        string
+            legalCurrency = transaction.Currency.ToString(),
+            legalOtherPartyName = transaction.OtherPartyName,
+            legalTimestamp = transaction.Timestamp.ToString();
+
+        // If amount is a Int32 string, it all goes well
+        foreach (var legalNumberAmount in new[] { "1", "69", "987654321", "-123456789", "420", "0", "-45781", Int32.MinValue.ToString(), Int32.MaxValue.ToString() })
+        {
+            var rawData = GenerateRawDataLine(legalNumberAmount, legalCurrency, legalOtherPartyName, legalTimestamp);
+            Should.NotThrow(() => parser.Parse(rawData));
+        }
+
+        // If amount is a number outside of the Int32 range, the parser throws
+        foreach (var outOfRangeNumberAmount in new[] { ((long)Int32.MaxValue + 1).ToString(), ((long)Int32.MinValue - 1).ToString(), "-99999999999", "99999999999" })
+        {
+            var rawData = GenerateRawDataLine(outOfRangeNumberAmount, legalCurrency, legalOtherPartyName, legalTimestamp);
+            var exception = Should.Throw<ArgumentException>(() => parser.Parse(rawData));
+            exception.Message.ShouldContain(outOfRangeNumberAmount.ToString());
+        }
+
+        // If amount is a not a number at all, the parser throws
+        foreach (var notANumberAmount in new[] { "one", "notARealNumber", "John Doe", "throw new ArgumentException()" })
+        {
+            var rawData = GenerateRawDataLine(notANumberAmount, legalCurrency, legalOtherPartyName, legalTimestamp);
+            var exception = Should.Throw<ArgumentException>(() => parser.Parse(rawData));
+            exception.Message.ShouldContain(notANumberAmount);
+        }
+    }
+
+    [Theory, MemberData(nameof(GenerateLegalTransactionObjects))]
+    public void CurrencyShouldAlwaysBeACurrency(Transaction transaction)
+    {
+        var parser = new MockCSVParser();
+
+        // Get legal values for all other fields
+        string
+            legalAmount = transaction.Amount.ToString(),
+            legalOtherPartyName = transaction.OtherPartyName,
+            legalTimestamp = transaction.Timestamp.ToString();
+
+        // If currency is a supported currency string, it all goes well
+        foreach (string legalCurrencyString in EnumExtensions.GetAllValues<Currency>().Select(currency => currency.ToString()))
+        {
+            var rawData = GenerateRawDataLine(legalAmount, legalCurrencyString, legalOtherPartyName, legalTimestamp);
+            Should.NotThrow(() => parser.Parse(rawData));
+        }
+
+        // If currency is a not a currency at all, the parser throws
+        foreach (var notACurrencyString in new[] { "NoCurrency", "No money, no problems", "EURALALALA", "I need a dollar" })
+        {
+            var rawData = GenerateRawDataLine(legalAmount, notACurrencyString, legalOtherPartyName, legalTimestamp);
+            Should.Throw<ArgumentException>(() => parser.Parse(rawData));
+        }
+    }
+
+    [Theory, MemberData(nameof(GenerateLegalTransactionObjects))]
+    public void TimestampShouldAlwaysBeADateTime(Transaction transaction)
+    {
+        var parser = new MockCSVParser();
+
+        // Get legal values for all other fields
+        string
+            legalAmount = transaction.Amount.ToString(),
+            legalCurrency = transaction.Currency.ToString(),
+            legalOtherPartyName = transaction.OtherPartyName;
+
+        // If timestamp is a DateTime string, it all goes well
+        foreach (var legalDateTimeString in new[] { "2025-01-15T15:15:12", "2024-10-06T10:30:45", "2024-11-12T01:02:03" })
+        {
+            var rawData = GenerateRawDataLine(legalAmount, legalCurrency, legalOtherPartyName, legalDateTimeString);
+            Should.NotThrow(() => parser.Parse(rawData));
+        }
+
+        // If amount is a not a number at all, the parser throws
+        foreach (var notADateTimeString in new[] { "new DateTime()", "Also not a date time string", "John Doe", "throw new ArgumentException()" })
+        {
+            var rawData = GenerateRawDataLine(legalAmount, legalCurrency, legalOtherPartyName, notADateTimeString);
+            Should.Throw<ArgumentException>(() => parser.Parse(rawData));
+        }
+    }
+
+    public static IEnumerable<object[]> GenerateLegalTransactionObjects()
+        => GenerateLegalTransactions().Select(transaction => new object[] { transaction });
+    public static IEnumerable<object[]> GenerateLegalStringAndTransactionObjects()
+        => GenerateLegalTransactions().Select(transaction => new object[] { transaction.RawData, transaction });
+    private static IEnumerable<Transaction> GenerateLegalTransactions()
+    {
         foreach (var amount in new int[] { 23, 12, -504, 1028 })
-            foreach (var currency in Enum.GetValues(typeof(Currency)).Cast<Currency>())
-                foreach (string otherParty in new[] { "OTHER PARTY", "John Doe", "Jack Sparrow" })
+            foreach (var currency in EnumExtensions.GetAllValues<Currency>())
+                foreach (string otherPartyName in new[] { "OTHER PARTY", "John Doe", "Jack Sparrow" })
                     foreach (var timestamp in new[] { new DateTime(2025, 01, 20, 18, 39, 12), new DateTime(2022, 12, 26, 06, 52, 37) })
-                        foreach (var category in Enum.GetValues(typeof(Category)).Cast<Category>())
-                        {
-                            var rawData = $"{amount}{sep}{currency}{sep}{otherParty}{sep}{timestamp}";
-                            yield return (rawData, new Transaction
+                        foreach (var category in EnumExtensions.GetAllValues<Category>())
+                            yield return (new Transaction
                             {
                                 Amount = amount,
                                 Currency = currency,
-                                OtherPartyName = otherParty,
+                                OtherPartyName = otherPartyName,
                                 Timestamp = timestamp,
-                                RawData = rawData,
+                                RawData = GenerateRawDataLine(amount.ToString(), currency.ToString(), otherPartyName, timestamp.ToString()),
                                 Category = category,
                             });
-                        }
     }
+
+    private static string GenerateRawDataLine(string amount, string currency, string otherPartyName, string timestamp, string separator = MockCSVParser.DefaultSeparator)
+        => $"{amount}{separator}{currency}{separator}{otherPartyName}{separator}{timestamp}";
 }
