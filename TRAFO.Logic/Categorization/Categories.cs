c3e1afc42cbce33784c47e11f27db21dbcca24f9
Loïse <izakhaak@gmail.com>
2025-02-12 18:44:31 +0100

    #29 setup default predicates

---
+++ TRAFO.Logic/Categorization/Categories.cs
@@ -0,0 +1,29 @@
+﻿using TRAFO.Logic.Categorization.Predicates;
+
+namespace TRAFO.Logic.Categorization;
+
+public static class Categories
+{
+    public const string Groceries = nameof(Groceries);
+    public const string Rent = nameof(Rent);
+
+    public static TransactionPredicate[] GetDefaultPredicates() => new TransactionPredicate[]
+    {
+        // todo #55: these should not be hardcoded here
+        new DescriptionContainsPredicate()
+        {
+            ContainedString = "AH TO GO",
+            LabelToSet = nameof(Groceries),
+        },
+        new DescriptionContainsPredicate()
+        {
+            ContainedString = "ALBERT HEIJN",
+            LabelToSet = nameof(Groceries),
+        },
+        new DescriptionContainsPredicate()
+        {
+            ContainedString = "JUMBO",
+            LabelToSet = nameof(Groceries),
+        },
+    };
+}
