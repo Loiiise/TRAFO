66fc0f45766bf4ae14924225ce79f323627e806a
Loïse <izakhaak@gmail.com>
2025-02-12 23:55:19 +0100

    #34 implemented ShowUncategorizedTransactionsCommand

--- TRAFO.Logic/Transaction/Transaction.cs
+++ TRAFO.Logic/Transaction/Transaction.cs
@@ -37,5 +37,15 @@ public sealed record Transaction
     private readonly string? _thisPartyName;
     private readonly string? _otherPartyName;
 
-    public override string ToString() => $"Transaction of {Amount} {Currency} on {Timestamp} from {ThisPartyIdentifier} to {OtherPartyIdentifier}";
+    public override string ToString() => $"Transaction of {ShowAmount()} {Currency} on {Timestamp} from {ThisPartyIdentifier} to {OtherPartyIdentifier} with description {Description}";
+
+    private string ShowAmount() => Currency is Currency.EUR
+        ? $"{Amount / 100},{GetEuroCentString(Amount % 100)}"
+        : Amount.ToString();
+
+    private string GetEuroCentString(long cents) 
+        => cents == 0 
+        ? "00"
+        : Math.Abs(cents).ToString();
+
 }
