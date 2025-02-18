861810d692af08bd19a65d09f389f05e7c7013b2
Loïse <izakhaak@gmail.com>
2025-02-18 18:25:30 +0100

    #35 setup command configurations more generic so they can be reused by flags

--- TRAFO.IO/Command/CommandMetaData.cs
+++ TRAFO.IO/Command/CommandMetaData.cs
@@ -1,18 +1,8 @@
 ﻿namespace TRAFO.IO.Command;
 
-public static class CommandMetaData
+internal class CommandMetaData : MetaData<CommandConfiguration>, IMetaDataCommandConfiguration
 {
-    public static IEnumerable<(string, string, string)> AllCommandNamesTagsAndDescriptions() => _commandConfigurations.Select(c => (c.Name, c.Tag, c.Description));
-    internal static IEnumerable<(string, string)> AllCommandTagsAndDescriptions() => _commandConfigurations.Select(c => (c.Tag, c.Description));
-
-    public static string GetNameFromTag(string commandTag) => _commandConfigurations.Single(c => c.Tag == commandTag).Name;
-    public static string GetTagFromName(string commandName) => _commandConfigurations.Single(c => c.Name == commandName).Tag;
-
-    public static string GetDescriptionFromTag(string commandTag) => _commandConfigurations.Single(c => c.Tag == commandTag).Description;
-    public static string GetDescriptionFromName(string commandName) => _commandConfigurations.Single(c => c.Name == commandName).Description;
-
-
-    private static CommandConfiguration[] _commandConfigurations = new[]
+    protected override CommandConfiguration[] _commandConfigurations => new[]
     {
         // todo #44: this is hardcoded for now but will be extracted to a file at some point
         new CommandConfiguration(nameof(HelpCommand), "help", "This is the help command."),
@@ -22,5 +12,3 @@ public static class CommandMetaData
         new CommandConfiguration(nameof(StatusCommand), "status", "Shows the current status of transaction categorization."),
     };
 }
-
-internal record CommandConfiguration(string Name, string Tag, string Description);
---
+++ TRAFO.IO/Command/Flags/FlagMetaData.cs
@@ -0,0 +1,11 @@
+﻿namespace TRAFO.IO.Command.Flags;
+
+internal class FlagMetaData : MetaData<FlagConfiguration>, IMetaDataFlagConfiguration
+{
+    protected override FlagConfiguration[] _commandConfigurations => new[]
+    {
+        // todo #44: this is hardcoded for now but will be extracted to a file at some point
+        new FlagConfiguration(string.Empty, "from", "start date of the range in a YYYY-MM-DD format."),
+        new FlagConfiguration(string.Empty, "till", "end date of the range in a YYYY-MM-DD format."),
+    };
+}
