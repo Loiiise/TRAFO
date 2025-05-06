f6326e93b4792bdc5e39ef6a92fde3b7485287d8
Loïse <45357831+Loiiise@users.noreply.github.com>
2025-05-06 21:16:39 +0200

    #21 setup balance record

---
+++ TRAFO.Logic/Balance.cs
@@ -0,0 +1,12 @@
+﻿namespace TRAFO.Logic;
+
+public sealed record Balance
+{
+    /// <summary>
+    /// In cents
+    /// </summary>
+    public required long Amount { get; init; }
+    public required Currency Currency { get; init; }
+    public required string ThisPartyIdentifier { get; init; }
+    public required DateTime Timestamp { get; init; }
+}
