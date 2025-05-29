e7cb305a8c8070a744f7a9bfbe67b1130e7d0bde
Loïse <45357831+Loiiise@users.noreply.github.com>
2025-05-29 11:44:21 +0200

    #67 added support for balances on labels and label categories

--- TRAFO.IO/Database/Entities/BalanceDatabaseEntry.cs
+++ TRAFO.IO/Database/Entities/BalanceDatabaseEntry.cs
@@ -4,13 +4,28 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace TRAFO.IO.Database.Entities;
 
 [PrimaryKey(nameof(BalanceId))]
-internal sealed class BalanceDatabaseEntry
+internal abstract class BalanceDatabaseEntry
 {
     public required Guid BalanceId { get; set; } = new();
     public required long Amount { get; set; }
-
-    [ForeignKey(nameof(AccountDatabaseEntry.AccountId))]
-    public required string ThisPartyAccountId { get; set; }
     public required DateTime Timestamp { get; set; }
     public required bool IsDerived { get; set; }
+}
+
+internal sealed class AccountBalanceDatabaseEntry : BalanceDatabaseEntry
+{
+    [ForeignKey(nameof(AccountDatabaseEntry.AccountId))]
+    public required string AccountId { get; set; }
+}
+
+internal sealed class LabelBalanceDatabaseEntry : BalanceDatabaseEntry
+{
+    [ForeignKey(nameof(LabelDatabaseEntry.LabelId))]
+    public required Guid LabelId { get; set; }
+}
+
+internal sealed class LabelCategoryBalanceDatabaseEntry : BalanceDatabaseEntry
+{
+    [ForeignKey(nameof(LabelCategoryDatabaseEntry.LabelCategoryId))]
+    public required Guid LabelCategoryId { get; set; }
 }
