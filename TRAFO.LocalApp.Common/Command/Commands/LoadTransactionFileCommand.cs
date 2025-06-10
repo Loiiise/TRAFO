0c2af5d0fe51e919b049e2e8edcd6fb780bf4cbe
Loiiise <loisehaak@gmail.com>
2025-06-10 18:46:05 +0200

    #49 finalized flow and implemented flag in cli project

--- TRAFO.LocalApp.Common/Command/Commands/LoadTransactionFileCommand.cs
+++ TRAFO.LocalApp.Common/Command/Commands/LoadTransactionFileCommand.cs
@@ -2,24 +2,33 @@
 using TRAFO.LocalApp.Common.Command.Flags;
 using TRAFO.LocalApp.Common.FileReading;
 using TRAFO.Logic.Categorization;
+using TRAFO.Logic.Extensions;
 using TRAFO.Repositories;
 
 namespace TRAFO.LocalApp.Common.Command;
 public class LoadTransactionFileCommand : FromTillCommand
 {
     public required FilePathArgument FilePathArgument { get; init; }
-    public required SkipFirstLineFlag? SkipFirstLineFlag { get; init; }
+    private TransactionFileReaderConfiguration _transactionFileReaderConfiguration { get; init; }
 
     public LoadTransactionFileCommand(ITransactionFileReader transactionFileReader, ILabelApplier[] labelers, ITransactionWriter transactionWriter, ICommandFlag[] flags) : base(flags)
     {
         _transactionFileReader = transactionFileReader;
         _labelers = labelers;
         _transactionWriter = transactionWriter;
+
+        var skipFirstLineFlag = flags.GetFirstOrDefault<SkipFirstLineFlag>();
+        _transactionFileReaderConfiguration = skipFirstLineFlag == null ?
+            new TransactionFileReaderConfiguration() :
+            new TransactionFileReaderConfiguration()
+            {
+                SkipFirstLine = skipFirstLineFlag.Value,
+            };
     }
 
     public override void Execute()
     {
-        var transactions = _transactionFileReader.ReadAllTransactions(FilePathArgument.Value);
+        var transactions = _transactionFileReader.ReadAllTransactions(FilePathArgument.Value, _transactionFileReaderConfiguration);
 
         if (_from is not null) transactions = transactions.Where(t => t.Timestamp >= _from);
         if (_till is not null) transactions = transactions.Where(t => t.Timestamp <= _till);
