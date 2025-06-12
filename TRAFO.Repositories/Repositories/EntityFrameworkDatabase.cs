620ec7fa983ea54a7d656edd9aa3f5cbbda48a31
Loïse <45357831+Loiiise@users.noreply.github.com>
2025-06-12 17:30:03 +0200

    #83 moved transaction database interfaces to separate repository

--- TRAFO.Repositories/Repositories/EntityFrameworkDatabase.cs
+++ TRAFO.Repositories/Repositories/EntityFrameworkDatabase.cs
@@ -5,34 +5,12 @@ using TRAFO.Repositories.Entities;
 namespace TRAFO.Repositories;
 
 public class EntityFrameworkDatabase :
-    ITransactionRepository,
     IBalanceRepository
 {
     public EntityFrameworkDatabase() : this(new DbContextOptions<EntityFrameworkDatabaseContext>()) { }
     public EntityFrameworkDatabase(DbContextOptions databaseContextOptions)
     {
         _context = new EntityFrameworkDatabaseContext(databaseContextOptions);
-    }
-    public IEnumerable<Transaction> ReadTransactionsInRange(DateTime? from, DateTime? till)
-    {
-        var transactions = ReadAllTransactions();
-
-        if (from is not null) transactions = transactions.Where(t => t.Timestamp >= from);
-        if (till is not null) transactions = transactions.Where(t => t.Timestamp <= till);
-
-        return transactions;
-    }
-
-    public void WriteTransaction(Transaction transaction)
-    {
-        _context.Transaction.Add(ToDatabaseEntry(transaction));
-        _context.SaveChanges();
-    }
-
-    public void WriteTransactions(IEnumerable<Transaction> transactions)
-    {
-        _context.Transaction.AddRange(transactions.Select(ToDatabaseEntry));
-        _context.SaveChanges();
     }
 
     public IEnumerable<Balance> ReadBalances(string identifier)
