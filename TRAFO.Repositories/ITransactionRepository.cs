92453a54c3149083a169d7b9467cb4a4aa27880f
Loïse <45357831+Loiiise@users.noreply.github.com>
2025-08-09 12:52:11 +0200

    #88 setup itransactionreader

--- TRAFO.Repositories/ITransactionRepository.cs
+++ TRAFO.Repositories/ITransactionRepository.cs
@@ -8,8 +8,28 @@ internal interface ITransactionRepository :
 
 public interface ITransactionReader
 {
-    public IEnumerable<Transaction> ReadAllTransactions();
-    public IEnumerable<Transaction> ReadTransactionsInRange(DateTime? from, DateTime? till);
+    public IEnumerable<Transaction> ReadTransactions();
+    public IEnumerable<Transaction> ReadTransactions(DateTime? from, DateTime? till);
+
+    public IEnumerable<Transaction> ReadTransactionsFromAccount(Account account);
+    public IEnumerable<Transaction> ReadTransactionsFromAccount(Account account, DateTime? from, DateTime? till);
+    public IEnumerable<Transaction> ReadTransactionsFromAccount(Guid accountId);
+    public IEnumerable<Transaction> ReadTransactionsFromAccount(Guid accountId, DateTime? from, DateTime? till);
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(Label label, bool includeChildTransactions = true);
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(Label label, DateTime? from, DateTime? till, bool includeChildTransactions = true);
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(Guid labelId, bool includeChildTransactions = true);
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(Guid labelId, DateTime? from, DateTime? till, bool includeChildTransactions = true);
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(IEnumerable<Label> labels);
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(IEnumerable<Label> labels, DateTime? from, DateTime? till);
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(IEnumerable<Guid> labelIds);
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(IEnumerable<Guid> labelIds, DateTime? from, DateTime? till);
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabelCategory(LabelCategory labelCategory);
+    public IEnumerable<Transaction> ReadTransactionsFromLabelCategory(LabelCategory labelCategory, DateTime? from, DateTime? till);
+    public IEnumerable<Transaction> ReadTransactionsFromLabelCategory(Guid labelCategoryId);
+    public IEnumerable<Transaction> ReadTransactionsFromLabelCategory(Guid labelCategoryId, DateTime? from, DateTime? till);
 }
 
 public interface ITransactionWriter
--- TRAFO.Repositories/Repositories/TransactionRepository.cs
+++ TRAFO.Repositories/Repositories/TransactionRepository.cs
@@ -3,30 +3,115 @@
 namespace TRAFO.Repositories;
 public sealed class TransactionRepository : EntityFrameworkDatabase, ITransactionRepository
 {
-    public IEnumerable<Transaction> ReadAllTransactions()
+    private IQueryable<Transaction> QueryTransactions() => _context.Transaction.Select(dbEntity => FromDatabaseEntry(dbEntity));
+    private IQueryable<Transaction> QueryTransactionsInRange(DateTime? from, DateTime? till)
+        => from == null && till == null ?
+            QueryTransactions() :
+            from == null ?
+                QueryTransactions().Where(t => t.Timestamp <= till) :
+                till == null ?
+                    QueryTransactions().Where(t => t.Timestamp >= from) :
+                    QueryTransactions()
+                        .Where(t => t.Timestamp <= till)
+                        .Where(t => t.Timestamp >= from);
+
+    public IEnumerable<Transaction> ReadTransactions()
     {
-        return _context.Transaction.Select(FromDatabaseEntry);
+        throw new NotImplementedException();
     }
 
-    public IEnumerable<Transaction> ReadTransactionsInRange(DateTime? from, DateTime? till)
+    public IEnumerable<Transaction> ReadTransactions(DateTime? from, DateTime? till)
     {
-        var transactions = ReadAllTransactions();
+        throw new NotImplementedException();
+    }
 
-        if (from is not null) transactions = transactions.Where(t => t.Timestamp >= from);
-        if (till is not null) transactions = transactions.Where(t => t.Timestamp <= till);
+    public IEnumerable<Transaction> ReadTransactionsFromAccount(Account account)
+    {
+        throw new NotImplementedException();
+    }
 
-        return transactions;
+    public IEnumerable<Transaction> ReadTransactionsFromAccount(Account account, DateTime? from, DateTime? till)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromAccount(Guid accountId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromAccount(Guid accountId, DateTime? from, DateTime? till)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(Label label, bool includeChildTransactions = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(Label label, DateTime? from, DateTime? till, bool includeChildTransactions = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(Guid labelId, bool includeChildTransactions = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(Guid labelId, DateTime? from, DateTime? till, bool includeChildTransactions = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(IEnumerable<Label> labels)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(IEnumerable<Label> labels, DateTime? from, DateTime? till)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(IEnumerable<Guid> labelIds)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabel(IEnumerable<Guid> labelIds, DateTime? from, DateTime? till)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabelCategory(LabelCategory labelCategory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabelCategory(LabelCategory labelCategory, DateTime? from, DateTime? till)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabelCategory(Guid labelCategoryId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> ReadTransactionsFromLabelCategory(Guid labelCategoryId, DateTime? from, DateTime? till)
+    {
+        throw new NotImplementedException();
     }
 
     public void WriteTransaction(Transaction transaction)
     {
-        _context.Transaction.Add(ToDatabaseEntry(transaction));
-        _context.SaveChanges();
+        throw new NotImplementedException();
     }
 
     public void WriteTransactions(IEnumerable<Transaction> transactions)
     {
-        _context.Transaction.AddRange(transactions.Select(ToDatabaseEntry));
-        _context.SaveChanges();
+        throw new NotImplementedException();
     }
 }
